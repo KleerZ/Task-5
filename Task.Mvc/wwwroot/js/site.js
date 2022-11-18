ec3db4fd560ce52ce8f1b3238907cd295b0697a5
KleerZ <escapeobzory@gmail.com>
2022-11-18 19:41:45 +0300

    add people generator page

--- Task.Mvc/wwwroot/js/site.js
+++ Task.Mvc/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿let errorRange = document.getElementById("error-range")
+errorRange.addEventListener('input', () => {
+    let errorInput = document.getElementById("error-input")
+    errorInput.value = errorRange.value
+})
